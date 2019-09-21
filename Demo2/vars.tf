dfcc745167a3a468554e4abc071e7b242ab45d0f
Singh <navkaran.singh@nuance.com>
2019-09-21 14:16:48 -0400

    Demo2 - splitting the project using variables

--- Demo2/vars.tf
+++ Demo2/vars.tf
@@ -4,9 +4,10 @@ variable "AWS_REGION"{
 	default = "us-east1"
 }
 variable "AMIS"{
+	type = "map"
 	default = {
-		us-east-1 = ""
-		us-west2 = ""
-		eu-west1 = ""
+  		us-east-1 = "ami-04763b3055de4860b"
+    	us-west-2 = "ami-0994c095691a46fb5"
+    	eu-west-1 = "ami-03ef731cc103c9f09"
 	}
 }
